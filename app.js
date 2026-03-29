c4fb33dd1ed83b0483b35fa0e468cd03d22bd3b0
Devansh <devanshd78@gmail.com>
2026-03-29 16:13:48 +0530

    updates

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ const server = http.createServer(app);
 const GridFSBucket = mongoose.mongo.GridFSBucket;
 const { Types } = mongoose;
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 5000;
 const JSON_LIMIT = process.env.JSON_LIMIT || "30mb";
 const GRIDFS_BUCKET_NAME = process.env.GRIDFS_BUCKET || "uploads";
 
